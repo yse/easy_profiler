2f833fc0a8e8de2b827eb2f3fb37cfa2933eec1d
Sergey Yagovtsev <yse.sey@gmail.com>
2016-07-31 19:20:58 +0300

    Add function getCurrentTime instead of std::chrono on windows

--- src/block.cpp
+++ src/block.cpp
@@ -28,11 +28,31 @@ Block::Block(const char* _name, color_t _color, block_type_t _type) :
 	thread_id = getCurrentThreadId();
 }
 
+inline timestamp_t getCurrentTime()
+{
+#ifdef WIN32
+	//see https://msdn.microsoft.com/library/windows/desktop/dn553408(v=vs.85).aspx
+	static LARGE_INTEGER frequency;
+	static bool first=true;
+	if (first)
+		QueryPerformanceFrequency(&frequency);
+	first = false;
+	LARGE_INTEGER elapsedMicroseconds;
+	if (!QueryPerformanceCounter(&elapsedMicroseconds))
+		return 0;
+	elapsedMicroseconds.QuadPart *= 1000000;
+	elapsedMicroseconds.QuadPart /= frequency.QuadPart;
+	return (timestamp_t)elapsedMicroseconds.QuadPart;
+#else
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds> time_point;
+	time_point = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+	return time_point.time_since_epoch().count();
+#endif
+}
+
 void BaseBlockData::tick(timestamp_t& stamp)
 {
-	std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> time_point;
-	time_point = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
-	stamp = time_point.time_since_epoch().count();
+	stamp = getCurrentTime();
 }
 
 Block::~Block()
