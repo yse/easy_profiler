b90e13509e0f1c5f75356a746349e748d228fe16
Victor Zarubkin <v.s.zarubkin@gmail.com>
2016-09-01 22:24:13 +0300

    small refactoring

--- include/profiler/reader.h
+++ include/profiler/reader.h
@@ -66,7 +66,7 @@ namespace profiler {
 
     //////////////////////////////////////////////////////////////////////////
 
-    class BlocksTree
+    class BlocksTree final
     {
         typedef BlocksTree This;
 
