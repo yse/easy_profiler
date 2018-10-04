3759ed628ac97a0e1ebe508e592595e3b54ab54a
evgeny <evgeny@eagle.ru>
2018-10-04 21:18:14 +0300

    Revert "Use thread_local instead of static in EASY_LOCAL_STATIC_PTR. As without it it's impossible to profile functions called from multiple threads."
    This reverts commit 3ee2067d7256975dedefd0e3fd41753447f8beb7.

--- easy_profiler_core/include/easy/details/easy_compiler_support.h
+++ easy_profiler_core/include/easy/details/easy_compiler_support.h
@@ -191,7 +191,7 @@ static_assert(false, "EasyProfiler is not configured for using your compiler typ
 #endif
 
 #ifndef EASY_LOCAL_STATIC_PTR
-# define EASY_LOCAL_STATIC_PTR(VarType, VarName, VarInitializer) thread_local VarType VarName = VarInitializer
+# define EASY_LOCAL_STATIC_PTR(VarType, VarName, VarInitializer) static VarType VarName = VarInitializer
 # define EASY_MAGIC_STATIC_AVAILABLE
 #endif
 
